c261933cfd803e522d6325237126b5b6e7933206
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-05-17 10:27:48 +0200

    Add Message and User model, validation, seeds for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "jana", password: "password")
+User.create(username: "petr", password: "password")
+User.create(username: "dylan", password: "password")
+User.create(username: "alex", password: "password")
+User.create(username: "sammy", password: "password")
